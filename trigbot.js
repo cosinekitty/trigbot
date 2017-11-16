72a66e42ac61eebf414b1a245cef7b0a3748ef09
Don Cross <cosinekitty@gmail.com>
2017-11-15 20:48:01 -0500

    Starting project.

---
+++ trigbot.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+    trigbot.js  -  Don Cross  -  15 November 2017.
+
+    A quiz to help learn to recognize which trig function to use
+    for a randomly-generated triangle.
+*/
+
+window.onload = function() {
+    var canvas = document.getElementById('GameCanvas');
+
+    function MakeRandomTriangle() {
+        
+    }
+}
